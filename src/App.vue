1e79b13a7410edcd0d46e52e24c48a91bad265c6
fcwgit <fcwgit@github.com>
2018-06-06 08:33:57 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <i class="icon iconfont icon-dianpu"></i>
+    <i class="icon iconfont icon-gongnengjianyi"></i>
+    <i class="icon iconfont icon-shouji"></i> -->
+    <leftNav></leftNav>
+    <div class="main">
+      <router-view/>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import leftNav from '@/components/common/leftNav'
+export default {
+  name: 'App',
+  components:{
+    leftNav
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+.main{
+  float: left;
+  width: 95%;
+  background-color: #EFF2F7;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
